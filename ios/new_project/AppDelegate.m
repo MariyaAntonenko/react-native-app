c32575b6c4650f20fb9d791907ca6d5171cd3bf9
Developer Maria <developermaria@Maria-MacBook-Air.local>
2021-12-22 17:35:35 +0200

    Task11_map

--- ios/new_project/AppDelegate.m
+++ ios/new_project/AppDelegate.m
@@ -11,7 +11,6 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-#import <GoogleMaps/GoogleMaps.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -28,7 +27,6 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-[GMSServices provideAPIKey:@"_YOUR_API_KEY_"];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {Screens} from './screens';
 import {LocalizationProvider} from './common/complicatedComponents/Context';
-import {StyledText} from './common/simpleComponents/Text';
 
 const App = () => {
   return (
     <LocalizationProvider>
-      {/*<Screens />*/}
-      <StyledText>APP</StyledText>
+      <Screens />
     </LocalizationProvider>
   );
 };
