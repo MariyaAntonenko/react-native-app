485bff13b82e0b476c606dabc7e1da95aeca5c5a
Developer Maria <developermaria@Maria-MacBook-Air.local>
2021-12-24 18:49:27 +0200

    task13_creation of fingerprint_fixing

--- src/screens/DrawerScreens/FingerprintScreen/FingerprintScreen.js
+++ src/screens/DrawerScreens/FingerprintScreen/FingerprintScreen.js
@@ -1,36 +1,57 @@
-import React, {useState} from 'react';
-import TouchID from 'react-native-touch-id';
+import React, {useEffect, useState} from 'react';
+import FingerprintScanner from 'react-native-fingerprint-scanner';
 import Fingerprint from '../../../assets/icons/fingerprint.svg';
 import {StyledText} from '../../../common/simpleComponents/Text';
 import {Block} from '../../../common/simpleComponents/Block';
 import {StyledButton} from '../../../common/simpleComponents/Button';
+import {StyledKeyboardAvoidingView} from '../../../common/simpleComponents/KeyboardAvoidingView';
+import {Alert} from 'react-native';
 export const FingerprintScreen = () => {
   const [checking, setChecking] = useState(false);
   const [fingerColor, setFingerColor] = useState('gray');
-  const optionalConfigObject = {
-    title: 'Authentication Required', // Android
-    imageColor: '#e00606', // Android
-    imageErrorColor: '#ff0000', // Android
-    sensorDescription: 'Touch sensor', // Android
-    sensorErrorDescription: 'Failed', // Android
-    cancelText: 'Cancel', // Android
-    fallbackLabel: 'Show Passcode', // iOS (if empty, then label is hidden)
-    unifiedErrors: false, // use unified error messages (default false)
-    passcodeFallback: false, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
-  };
-  const readFingerprint = () => {
-    setFingerColor('blue');
-    setChecking(true);
-    TouchID.authenticate('To continue work with app')
-      .then(success => {
-        setChecking(false);
-        setFingerColor('green');
+  const [biometry, setBiometry] = useState(null);
+
+  useEffect(() => {
+    FingerprintScanner.isSensorAvailable()
+      .then(biometryType => {
+        setBiometry({biometryType});
+        // FingerprintScanner.release();
       })
       .catch(error => {
-        setChecking(false);
-        setFingerColor('red');
+        console.log({errorMessage: error.message});
+        setFingerColor('gray');
+        // Alert.prompt('Enter your password', '', () => {
+        //   setFingerColor('green');
+        //   setChecking(false);
+        // });
+        // FingerprintScanner.release();
       });
+  }, [biometry]);
+
+  const readFingerprint = () => {
+    if (biometry !== null && biometry !== undefined) {
+      setFingerColor('blue');
+      setChecking(true);
+
+      FingerprintScanner.authenticate({
+        description: 'Scan your fingerprint',
+        fallbackEnabled: true,
+      })
+        .then(() => {
+          setFingerColor('green');
+          setChecking(false);
+          FingerprintScanner.release();
+        })
+        .catch(error => {
+          console.log(error.message);
+          setFingerColor('red');
+          setChecking(false);
+
+          FingerprintScanner.release();
+        });
+    }
   };
+
   return (
     <Block flex={1} alignItems={'center'} justifyContent={'flex-start'}>
       <Block pt={'15%'} pb={'10%'}>
@@ -41,13 +62,12 @@ export const FingerprintScreen = () => {
           </StyledText>
         )}
       </Block>
-
       <StyledButton
         onPress={readFingerprint}
         border={'0.5px solid gray'}
         borderRadius={'6px'}
         padding={'5%'}>
-        <StyledText color={'black'}>Touch ID</StyledText>
+        <StyledText color={'black'}>Touch to start scanning</StyledText>
       </StyledButton>
     </Block>
   );
