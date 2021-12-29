a40e7cc2f0fd4f54ad3584453a7d267eaa38473a
Developer Maria <developermaria@Maria-MacBook-Air.local>
2021-12-29 14:41:37 +0200

    Creation of App's icon and splash screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import 'react-native-gesture-handler';
 import {Screens} from './screens';
 import {LocalizationProvider} from './common/complicatedComponents/Context';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <LocalizationProvider>
       <Screens />
